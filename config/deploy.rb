415a326f6af3ab34a0541992d91e991d3cf5a590
Galina <Springstorm666@gmail.com>
2017-02-19 19:07:49 +0300

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,23 +4,29 @@ require 'mina/git'
 require 'mina/rvm'
 
 set :domain, 'lvee.org'
-set :deploy_to, '/home/lvee/engine/'
+set :deploy_to, "/home/lvee/engine"
 set :repository, 'https://github.com/lvee/lvee-engine.git'
-set :branch, 'staging'
-
+set :app_path,   "#{fetch(:current_path)}"
+set :branch, 'master'
+set :shared_paths, ['log/', 'tmp/', 'public/']
+set :shared_files, ["config/database.yml"]
 set :user, 'lvee'
-
-set :rails_env, 'production'
+#set :rails_env, 'production'
 #set :port, '22'
 #set :ssh_options, '-A'
 
 task :environment do
-  #invoke :'rbenv:load'
+	#queue "echo 'test'"
+  #command "cat #{fetch(:app_path)}/.ruby-version"
+  invoke :'rvm:use', 'ruby-2.3.3@default'
 end
 
-task deploy: :environment do
+task :deploy do
   deploy do
     invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
+    command "#{fetch (:bundle_prefix)} rake bootstrap"
+   invoke :'rails:assets_precompile'
   end
 end

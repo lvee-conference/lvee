35aa0a90aeeafabb32ee158c73cf254632e4bcc9
Alexandr D <alexandr.dubitsky@gmail.com>
2017-12-27 19:42:57 +0300

    add server tag

--- config/puma.rb
+++ config/puma.rb
@@ -16,3 +16,5 @@ stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.lo
 # Set master PID and state locations
 pidfile "#{tmp_dir}/pids/puma.pid"
 state_path "#{tmp_dir}/pids/puma.state"
+
+tag 'LVEE.org server'

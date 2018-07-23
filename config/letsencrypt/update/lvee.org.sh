d94f09cb9aef5153fd41420b63988bd3d8a022ca
pasha chebotarev <che.pasha@gmail.com>
2018-07-23 21:24:32 +0300

    Nginx (#30)
    * NGinx configs has been defined
    
    * Let's Encrypt scripts has been defined
    
    * NGinx configs has been optimized
    
    * HTML headers has been optimized
    
    * NGinx headers has been optimized

---
+++ config/letsencrypt/update/lvee.org.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+/home/lvee/letsencrypt/certbot/certbot-auto certonly \
+    --standalone \
+    --email info@lvee.org \
+    --renew-by-default \
+    --rsa-key-size 4096 \
+    -d lvee.org \
+    -d www.lvee.org \
+    -d lists.lvee.org \
+    -d www.lists.lvee.org \
+    -d summer.lvee.org \
+    -d winter.lvee.org \
+    --pre-hook 'systemctl stop nginx' \
+    --post-hook 'systemctl start nginx'

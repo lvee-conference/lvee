9bcc3c1d9d0b8c513bf550d789cd18d9e7bde2fe
che <che.pasha@gmail.com>
2018-07-27 15:48:44 +0300

    NGinx config has been optimized

--- config/letsencrypt/update/lvee.by.sh
+++ config/letsencrypt/update/lvee.by.sh
@@ -7,8 +7,8 @@
     --rsa-key-size 4096 \
     -d lvee.by \
     -d www.lvee.by \
+    -d mail.lvee.by \
     -d lists.lvee.by \
-    -d www.lists.lvee.by \
     -d summer.lvee.by \
     -d winter.lvee.by \
     --pre-hook 'systemctl stop nginx' \
